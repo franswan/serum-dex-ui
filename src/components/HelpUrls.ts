c423051aba10badaacaa961e331756304a09a265
Fran Swan <71414784+franswan@users.noreply.github.com>
2021-05-24 04:40:51 -0700

    Update HelpUrls.ts

--- src/components/HelpUrls.ts
+++ src/components/HelpUrls.ts
@@ -3,9 +3,9 @@ export const helpUrls = {
   customerSupportZh: 'https://t.me/ProjectSerum_Chinese',
   contactEmail: 'mailto:contact@projectserum.com',
   discord: 'https://discord.gg/EDvudv6',
-  telegram: 'https://t.me/ProjectSerum',
+  telegram: 'https://t.me/pupprtoken',
   github: 'https://github.com/project-serum',
   projectSerum: 'https://projectserum.com/',
-  developerResources: 'https://projectserum.com/developer-resources',
+  pupprTown: 'https://puppr.town',
   solanaBeach: 'https://solanabeach.io',
 };
