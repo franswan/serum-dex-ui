d09cca7f5b13dfe2a5cc533b73002574cc793397
Fran Swan <71414784+franswan@users.noreply.github.com>
2021-05-24 04:40:56 -0700

    Update TopBar.tsx

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -142,7 +142,7 @@ export default function TopBar() {
       <Wrapper>
         <LogoWrapper onClick={() => history.push(tradePageUrl)}>
           <img src={logo} alt="" />
-          {'SERUM'}
+          {'PIP-DEX'}
         </LogoWrapper>
         <Menu
           mode="horizontal"
